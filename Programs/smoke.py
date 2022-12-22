696825f236664af642bbcec6050c1f79aa721d92
Arjun Hegde <arjunmeega28@gmail.com>
2022-12-22 20:16:27 +0530

    Update smoke.py

--- Programs/smoke.py
+++ Programs/smoke.py
@@ -1,18 +1,14 @@
-from machine import Pin, ADC
-from time import sleep
-
-smoke = ADC(0)
-buzzer=Pin(12,Pin.OUT)
-
-while True:
-  smoke_value = smoke.read()
-  print(smoke_value)
-  if smoke_value > 550:
-    buzzer.value(1)
-    sleep(0.5)
-  else:
-    buzzer.value(0)
-    sleep(0.5)
-    
-
-
+from machine 
+import Pin, ADC 
+from time 
+import sleep 
+smoke = ADC(Pin(26)) # Smoke Sensor –GPIO26 
+smoke.atten(ADC.ATTN_11DB) # Full range: 3.3v 
+buzzer = Pin(14,Pin.OUT) 
+while True: 
+  smoke_value = smoke.read() 
+  print(smoke_value) 
+  sleep(1.0) 
+  if smoke_value > 50: 
+    buzzer.value(1) 
+    else: buzzer.value(0)
